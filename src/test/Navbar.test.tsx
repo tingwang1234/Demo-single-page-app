4d3b0034dddd1f32233bfb4a5228a607039a1e75
Ting Wang <ting.wang@thoughtworks.com>
2025-02-12 23:54:40 +0800

    Test accessibility

--- src/test/Navbar.test.tsx
+++ src/test/Navbar.test.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import {render, screen} from "@testing-library/react";
+import { axe } from 'vitest-axe';
 import {BrowserRouter} from "react-router-dom";
 import {describe} from "vitest";
 import Navbar from "../components/Navbar.tsx";
+
 
 const renderWithRouter = (ui: React.ReactElement) => {
     return render(<BrowserRouter>{ui}</BrowserRouter>);
 };
 
 describe("Navbar component", () => {
-    it("renders the logo", () => {
-        renderWithRouter(<Navbar />);
+    it("renders the logo", async () => {
+        renderWithRouter(<Navbar/>);
         expect(screen.getByText('DEMO SINGLE PAGE APP')).toBeInTheDocument();
     });
 
@@ -24,5 +26,12 @@ describe("Navbar component", () => {
         renderWithRouter(<Navbar />);
         expect(screen.getByText('Home').closest("a")).toHaveAttribute("href", "/");
         expect(screen.getByText('Test Page').closest("a")).toHaveAttribute("href", "/test");
+    });
+
+    it("should not have any accessibility violations", async () => {
+        const { container } = renderWithRouter(<Navbar />);
+        const results = await axe(container);
+
+        expect(results.violations.length).toBe(0);
     });
 })
--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,7 @@
 import '@testing-library/jest-dom'
+import { vi } from 'vitest';
+
+// Mock HTMLCanvasElement.prototype.getContext
+vi.stubGlobal('HTMLCanvasElement', class {
+    getContext = vi.fn();
+});
